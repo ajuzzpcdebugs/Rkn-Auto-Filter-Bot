5846a9c3eb4f41fe0217a3b44775f83b57445242
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2023-07-03 13:09:27 +0545

    Uploaded ExtraMods

---
+++ plugins/ExraMods/check_alive.py
@@ -0,0 +1,18 @@
+import time
+import random
+from pyrogram import Client, filters
+
+CMD = ["/", "."]
+
+@Client.on_message(filters.command("alive", CMD))
+async def check_alive(_, message):
+    await message.reply_text("Not dead but still here.. You have no love for me now. Wow.. you are not the same as before.. you have changed..😔 At least start coughing once in a while..🙂")
+
+
+@Client.on_message(filters.command("ping", CMD))
+async def ping(_, message):
+    start_t = time.time()
+    rm = await message.reply_text("...")
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
